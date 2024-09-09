00fa5171e740e7c158ed52e7fdd92200d8ca34f0
Saksham Man Shrestha <sakshammanshrestha@gmail.com>
2024-09-09 14:39:44 +0545

    kept the time option and set the main page

--- clock/src/App.jsx
+++ clock/src/App.jsx
@@ -1,15 +1,26 @@
-import DigitalClock from "./DigitalClock"
-import Stopwatch from "./Stopwatch"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import DigitalClock from "./DigitalClock";
+import Stopwatch from "./Stopwatch";
+import Timer from "./Timer";
 
 function App() {
-  
-  return(
+  return (
+    <Router>
+      <div>
+        <h1>Welcome to the Clock</h1>
 
-    <DigitalClock/>,
-    <Stopwatch/>
+        {/* Navigation Links */}
+        <Link to="/digital-clock">Digital Clock</Link> | <Link to="/stopwatch">Stopwatch</Link> | <Link to="/timer">Timer</Link>
 
+        {/* Routes */}
+        <Routes>
+          <Route path="/digital-clock" element={<DigitalClock />} />
+          <Route path="/stopwatch" element={<Stopwatch />} />
+          <Route path="/timer" element={<Timer />} />
+        </Routes>
+      </div>
+    </Router>
   );
-
 }
 
-export default App
+export default App;
